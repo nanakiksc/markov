be5760dfec5d4a2f24ce90f32dcc86b9aa0749bd
nanakiksc <polcusco@gmail.com>
2014-03-18 12:00:05 +0100

    Added MarkovChain as class. Removed State as class.

--- markov_chain.py
+++ markov_chain.py
@@ -5,52 +5,90 @@
 # Implement transition matrix.
 from random import random
 
-class State:
+class MarkovChain:
     """
-    Create a hidden state with an emission probability 'ep' and a transition
-    probability 'tp' to state 'S' '{S1: tp1, ..., Sn: tpn}.' The self.active
-    attribute indicates wich state is the Markov Chain currenlty in.
+    Create a Markov Chain that stores hidden states with different emission
+    probabilities and transition probabilities.
     """
 
-    def __init__(self, name, emission_probability, transition_probability):
+    def __init__(self):
         """
-        type(ep) and type(tp) must be float.
         """
         
-        self.name = name
-        self.ep = float(emission_probability)
-        self.tp = float(transition_probability)
-        self.is_active = False
+        self.emission_probs = []
+        self.transition_matrix = []
+        self.num_states = 0
+        self.current_state = None
+
+    def add_state(self, emission_probability, transition_probability):
+        """
+        Each state is added as a list or tuple of length 2. The first element is
+        the emission probability of the state. The second element is the row of
+        the transition matrix corresponding to the state.
+        """
+
+        ep = float(emission_probability)
+        self.emission_probs.append(ep)
+
+        tp = [float(p) for p in transition_probability]
+        self.transition_matrix.append(tp)
+        
+        self.num_states += 1
+
+    def check_transition_matrix(self):
+        """
+        Check whether the size and values of the transition matrix make sense.
+        """
+
+        for i, state in enumerate(self.transition_matrix):
+            assert sum(state) == 1
+            assert len(state) == self.num_states
+        assert i + 1 == self.num_states
 
     def emit(self):
-        if random() < self.ep:
+        """
+        Make an emission based on the emission probability of the current state.
+        """
+
+        if random() < self.emission_probs[self.current_state]:
             return 1
         else:
             return 0
 
-    def transit(self, state_list):
-        if random() < self.tp:
-            self.is_active = False
-            for state in state_list:
-                state.is_active = True
+    def transit(self):
+        """
+        Update current state by choosing a state 'st', with probability 'pr',
+        among all possible states, including the current state itself.
+        """
+        
+        r = random()
+        acc = 0
+        for st, pr in enumerate(self.transition_matrix[self.current_state]):
+            if acc <= r < pr:
+                self.current_state = st
+            acc += pr
 
-    def update(self, state_list):
-        if self.is_active:
+    def run(self):
+        """
+        Check that everything makes sense, initialize the chain and run it.
+        """
+        
+        chain.check_transition_matrix()
+
+        # Initalize current state of the Markov Chain.
+        chain.current_state = int(random() * self.num_states)
+
+        while True:
             print self.emit()
-            self.transit(state_list)
+            self.transit()
 
 
 if __name__ == '__main__':
+    
+    chain = MarkovChain()
 
-    # Create the (not so) Hidden States.
-    s1 = State('s1', 0.9, 0.2)
-    s2 = State('s2', 0.2, 0.1)
-
-    state_list = [s1, s2]
-
-    # Initalize the Markov Chain.
-    s1.is_active = True
-
-    while True:
-        for state in state_list:
-            state.update(state_list)
+    # Add the (not so) Hidden States.
+    chain.add_state(0.8, (0.9, 0.1)) # State 0
+    chain.add_state(0.05, (0.15, 0.85)) # State 1
+    
+    chain.run()
